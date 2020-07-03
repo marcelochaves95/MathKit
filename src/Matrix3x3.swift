a157dd6d2860cbe8d5d554685727640b8beb8c73
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-03 18:30:03 -0300

    Update Matrix3x3

--- src/Matrix3x3.swift
+++ src/Matrix3x3.swift
@@ -1,10 +1,3 @@
-//
-//  Matrix3x3.swift
-//  MathKit
-//
-//  Created by Marcelo Chaves on 04/05/19.
-//  Copyright © 2019 Marcelo Chaves. All rights reserved.
-
 import Foundation
 
 public struct Matrix3x3 {
@@ -30,7 +23,6 @@ extension Matrix3x3: Hashable {
 
 public extension Matrix3x3 {
     public static let identity = Matrix3x3(1, 0, 0, 0, 1, 0, 0, 0, 1)
-    
     public init(_ m11: Scalar, _ m12: Scalar, _ m13: Scalar,
                 _ m21: Scalar, _ m22: Scalar, _ m23: Scalar,
                 _ m31: Scalar, _ m32: Scalar, _ m33: Scalar) {
@@ -44,7 +36,7 @@ public extension Matrix3x3 {
         self.m32 = m32
         self.m33 = m33
     }
-    
+
     public init(scale: Vector2) {
         self.init(
             scale.x, 0, 0,
@@ -52,7 +44,7 @@ public extension Matrix3x3 {
             0, 0, 1
         )
     }
-    
+
     public init(translation: Vector2) {
         self.init(
             1, 0, 0,
@@ -60,7 +52,7 @@ public extension Matrix3x3 {
             translation.x, translation.y, 1
         )
     }
-    
+
     public init(rotation radians: Scalar) {
         let cs = cos(radians)
         let sn = sin(radians)
@@ -70,16 +62,15 @@ public extension Matrix3x3 {
             0, 0, 1
         )
     }
-    
+
     public init(_ m: [Scalar]) {
-        assert(m.count == 9, "array must contain 9 elements, contained \(m.count)")
         self.init(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8])
     }
-    
+
     public func toArray() -> [Scalar] {
         return [m11, m12, m13, m21, m22, m23, m31, m32, m33]
     }
-    
+
     public var adjugate: Matrix3x3 {
         return Matrix3x3(
             m22 * m33 - m23 * m32,
@@ -93,20 +84,20 @@ public extension Matrix3x3 {
             m11 * m22 - m12 * m21
         )
     }
-    
+
     public var determinant: Scalar {
         return (m11 * m22 * m33 + m12 * m23 * m31 + m13 * m21 * m32)
-            - (m13 * m22 * m31 + m11 * m23 * m32 + m12 * m21 * m33)
+             - (m13 * m22 * m31 + m11 * m23 * m32 + m12 * m21 * m33)
     }
-    
+
     public var transpose: Matrix3x3 {
         return Matrix3x3(m11, m21, m31, m12, m22, m32, m13, m23, m33)
     }
-    
+
     public var inverse: Matrix3x3 {
         return adjugate * (1 / determinant)
     }
-    
+
     public func interpolated(with m: Matrix3x3, by t: Scalar) -> Matrix3x3 {
         return Matrix3x3(
             m11 + (m.m11 - m11) * t,
@@ -120,11 +111,11 @@ public extension Matrix3x3 {
             m33 + (m.m33 - m33) * t
         )
     }
-    
+
     public static prefix func -(m: Matrix3x3) -> Matrix3x3 {
         return m.inverse
     }
-    
+
     public static func *(lhs: Matrix3x3, rhs: Matrix3x3) -> Matrix3x3 {
         return Matrix3x3(
             lhs.m11 * rhs.m11 + lhs.m21 * rhs.m12 + lhs.m31 * rhs.m13,
@@ -138,15 +129,15 @@ public extension Matrix3x3 {
             lhs.m13 * rhs.m31 + lhs.m23 * rhs.m32 + lhs.m33 * rhs.m33
         )
     }
-    
+
     public static func *(lhs: Matrix3x3, rhs: Vector2) -> Vector2 {
         return rhs * lhs
     }
-    
+
     public static func *(lhs: Matrix3x3, rhs: Vector3) -> Vector3 {
         return rhs * lhs
     }
-    
+
     public static func *(lhs: Matrix3x3, rhs: Scalar) -> Matrix3x3 {
         return Matrix3x3(
             lhs.m11 * rhs, lhs.m12 * rhs, lhs.m13 * rhs,
@@ -154,30 +145,84 @@ public extension Matrix3x3 {
             lhs.m31 * rhs, lhs.m32 * rhs, lhs.m33 * rhs
         )
     }
-    
+
     public static func ==(lhs: Matrix3x3, rhs: Matrix3x3) -> Bool {
-        if !lhs.m11.isEqual(to: rhs.m11) { return false }
-        if !lhs.m12.isEqual(to: rhs.m12) { return false }
-        if !lhs.m13.isEqual(to: rhs.m13) { return false }
-        if !lhs.m21.isEqual(to: rhs.m21) { return false }
-        if !lhs.m22.isEqual(to: rhs.m22) { return false }
-        if !lhs.m23.isEqual(to: rhs.m23) { return false }
-        if !lhs.m31.isEqual(to: rhs.m31) { return false }
-        if !lhs.m32.isEqual(to: rhs.m32) { return false }
-        if !lhs.m33.isEqual(to: rhs.m33) { return false }
+        if !lhs.m11.isEqual(to: rhs.m11) {
+            return false
+        }
+
+        if !lhs.m12.isEqual(to: rhs.m12) {
+            return false
+        }
+
+        if !lhs.m13.isEqual(to: rhs.m13) {
+            return false
+        }
+
+        if !lhs.m21.isEqual(to: rhs.m21) {
+            return false
+        }
+
+        if !lhs.m22.isEqual(to: rhs.m22) {
+            return false
+        }
+
+        if !lhs.m23.isEqual(to: rhs.m23) {
+            return false
+        }
+
+        if !lhs.m31.isEqual(to: rhs.m31) {
+            return false
+        }
+
+        if !lhs.m32.isEqual(to: rhs.m32) {
+            return false
+        }
+
+        if !lhs.m33.isEqual(to: rhs.m33) {
+            return false
+        }
+
         return true
     }
     
     public static func !=(lhs: Matrix3x3, rhs: Matrix3x3) -> Bool {
-        if lhs.m11.isEqual(to: rhs.m11) { return false }
-        if lhs.m12.isEqual(to: rhs.m12) { return false }
-        if lhs.m13.isEqual(to: rhs.m13) { return false }
-        if lhs.m21.isEqual(to: rhs.m21) { return false }
-        if lhs.m22.isEqual(to: rhs.m22) { return false }
-        if lhs.m23.isEqual(to: rhs.m23) { return false }
-        if lhs.m31.isEqual(to: rhs.m31) { return false }
-        if lhs.m32.isEqual(to: rhs.m32) { return false }
-        if lhs.m33.isEqual(to: rhs.m33) { return false }
+        if lhs.m11.isEqual(to: rhs.m11) {
+            return false
+        }
+
+        if lhs.m12.isEqual(to: rhs.m12) {
+            return false
+        }
+
+        if lhs.m13.isEqual(to: rhs.m13) {
+            return false
+        }
+
+        if lhs.m21.isEqual(to: rhs.m21) {
+            return false
+        }
+
+        if lhs.m22.isEqual(to: rhs.m22) {
+            return false
+        }
+
+        if lhs.m23.isEqual(to: rhs.m23) {
+            return false
+        }
+
+        if lhs.m31.isEqual(to: rhs.m31) {
+            return false
+        }
+
+        if lhs.m32.isEqual(to: rhs.m32) {
+            return false
+        }
+
+        if lhs.m33.isEqual(to: rhs.m33) {
+            return false
+        }
+
         return true
     }
 }
