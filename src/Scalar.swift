81e29d610efda9c4487ae0d922170fccc3565461
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-03 18:55:04 -0300

    Update Scalar

--- src/Scalar.swift
+++ src/Scalar.swift
@@ -1,11 +1,3 @@
-//
-//  Scalar.swift
-//  MathKit
-//
-//  Created by Marcelo Chaves on 04/05/19.
-//  Copyright © 2019 Marcelo Chaves. All rights reserved.
-//
-
 import Foundation
 
 public typealias Scalar = Float
@@ -17,11 +9,11 @@ public extension Scalar {
     public static let degreesPerRadian = 180 / pi
     public static let radiansPerDegree = pi / 180
     public static let epsilon: Scalar = 0.0001
-    
+
     public static func !=(lhs: Scalar, rhs: Scalar) -> Bool {
         return abs(lhs - rhs) < .epsilon
     }
-    
+
     fileprivate var sign: Scalar {
         return self > 0 ? 1 : -1
     }
