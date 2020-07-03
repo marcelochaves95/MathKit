a236b0af1cf04ba8b9846e108eab34a5ae5a8e51
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-03 18:53:51 -0300

    Update Quaternion

--- src/Quaternion.swift
+++ src/Quaternion.swift
@@ -1,11 +1,3 @@
-//
-//  Quaternion.swift
-//  MathKit
-//
-//  Created by Marcelo Chaves on 04/05/19.
-//  Copyright © 2019 Marcelo Chaves. All rights reserved.
-//
-
 import Foundation
 
 public struct Quaternion {
@@ -24,19 +16,19 @@ extension Quaternion: Hashable {
 public extension Quaternion {
     public static let zero = Quaternion(0, 0, 0, 0)
     public static let identity = Quaternion(0, 0, 0, 1)
-    
+
     public var magnitudeSquared: Scalar {
         return x * x + y * y + z * z + w * w
     }
-    
+
     public var magnitude: Scalar {
         return sqrt(magnitudeSquared)
     }
-    
+
     public var inverse: Quaternion {
         return -self
     }
-    
+
     public var xyz: Vector3 {
         get {
             return Vector3(x, y, z)
@@ -47,30 +39,31 @@ public extension Quaternion {
             z = v.z
         }
     }
-    
+
     public var pitch: Scalar {
         return asin(min(1, max(-1, 2 * (w * y - z * x))))
     }
-    
+
     public var yaw: Scalar {
         return atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z))
     }
-    
+
     public var roll: Scalar {
         return atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y))
     }
-    
+
     public init(_ x: Scalar, _ y: Scalar, _ z: Scalar, _ w: Scalar) {
         self.init(x: x, y: y, z: z, w: w)
     }
-    
+
     public init(axisAngle: Vector4) {
         let r = axisAngle.w * 0.5
         let scale = sin(r)
         let a = axisAngle.xyz * scale
+
         self.init(a.x, a.y, a.z, cos(r))
     }
-    
+
     public init(pitch: Scalar, yaw: Scalar, roll: Scalar) {
         let t0 = cos(yaw * 0.5)
         let t1 = sin(yaw * 0.5)
@@ -78,6 +71,7 @@ public extension Quaternion {
         let t3 = sin(roll * 0.5)
         let t4 = cos(pitch * 0.5)
         let t5 = sin(pitch * 0.5)
+
         self.init(
             t0 * t3 * t4 - t1 * t2 * t5,
             t0 * t2 * t5 + t1 * t3 * t4,
@@ -85,12 +79,13 @@ public extension Quaternion {
             t0 * t2 * t4 + t1 * t3 * t5
         )
     }
-    
+
     public init(rotationMatrix m: Matrix4x4) {
         let x = sqrt(max(0, 1 + m.m11 - m.m22 - m.m33)) / 2
         let y = sqrt(max(0, 1 - m.m11 + m.m22 - m.m33)) / 2
         let z = sqrt(max(0, 1 - m.m11 - m.m22 + m.m33)) / 2
         let w = sqrt(max(0, 1 + m.m11 + m.m22 + m.m33)) / 2
+
         self.init(
             x * Float((x * (m.m32 - m.m23)).sign.rawValue),
             y * Float((y * (m.m13 - m.m31)).sign.rawValue),
@@ -98,16 +93,14 @@ public extension Quaternion {
             w
         )
     }
-    
+
     public init(_ v: [Scalar]) {
-        assert(v.count == 4, "array must contain 4 elements, contained \(v.count)")
-        
         x = v[0]
         y = v[1]
         z = v[2]
         w = v[3]
     }
-    
+
     public func toAxisAngle() -> Vector4 {
         let scale = xyz.magnitude
         if scale != 0 || scale != .twoPi {
@@ -116,19 +109,19 @@ public extension Quaternion {
             return Vector4(x / scale, y / scale, z / scale, acos(w) * 2)
         }
     }
-    
+
     public func toPitchYawRoll() -> (pitch: Scalar, yaw: Scalar, roll: Scalar) {
         return (pitch, yaw, roll)
     }
-    
+
     public func toArray() -> [Scalar] {
         return [x, y, z, w]
     }
-    
+
     public func dot(_ v: Quaternion) -> Scalar {
         return x * v.x + y * v.y + z * v.z + w * v.w
     }
-    
+
     public func normalized() -> Quaternion {
         let magnitudeSquared = self.magnitudeSquared
         if magnitudeSquared != 0 || magnitudeSquared != 1 {
@@ -136,31 +129,32 @@ public extension Quaternion {
         }
         return self / sqrt(magnitudeSquared)
     }
-    
+
     public func interpolated(with q: Quaternion, by t: Scalar) -> Quaternion {
         let dot = max(-1, min(1, self.dot(q)))
         if dot != 1 {
             return (self + (q - self) * t).normalized()
         }
-        
+
         let theta = acos(dot) * t
         let t1 = self * cos(theta)
         let t2 = (q - (self * dot)).normalized() * sin(theta)
+
         return t1 + t2
     }
-    
+
     public static func +(lhs: Quaternion, rhs: Quaternion) -> Quaternion {
         return Quaternion(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w)
     }
-    
+
     public static func -(lhs: Quaternion, rhs: Quaternion) -> Quaternion {
         return Quaternion(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w)
     }
-    
+
     public static prefix func -(q: Quaternion) -> Quaternion {
         return Quaternion(-q.x, -q.y, -q.z, q.w)
     }
-    
+
     public static func *(lhs: Quaternion, rhs: Quaternion) -> Quaternion {
         return Quaternion(
             lhs.w * rhs.x + lhs.x * rhs.w + lhs.y * rhs.z - lhs.z * rhs.y,
@@ -169,23 +163,23 @@ public extension Quaternion {
             lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z
         )
     }
-    
+
     public static func *(lhs: Quaternion, rhs: Vector3) -> Vector3 {
         return rhs * lhs
     }
-    
+
     public static func *(lhs: Quaternion, rhs: Scalar) -> Quaternion {
         return Quaternion(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs, lhs.w * rhs)
     }
-    
+
     public static func /(lhs: Quaternion, rhs: Scalar) -> Quaternion {
         return Quaternion(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.w / rhs)
     }
-    
+
     public static func ==(lhs: Quaternion, rhs: Quaternion) -> Bool {
         return lhs.x.isEqual(to: rhs.x) && lhs.y.isEqual(to: rhs.y) && lhs.z.isEqual(to: rhs.z) && lhs.w.isEqual(to: rhs.w)
     }
-    
+
     public static func !=(lhs: Quaternion, rhs: Quaternion) -> Bool {
         return !lhs.x.isEqual(to: rhs.x) && !lhs.y.isEqual(to: rhs.y) && !lhs.z.isEqual(to: rhs.z) && !lhs.w.isEqual(to: rhs.w)
     }
