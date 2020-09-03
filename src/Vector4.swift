a191ab80070f7ad16098a4482809042b320a94dc
Marcelo Chaves <marcelochaves17@gmail.com>
2020-09-03 19:59:50 -0300

    Update Vector4

--- src/Vector4.swift
+++ src/Vector4.swift
@@ -1,11 +1,3 @@
-//
-//  Vector4.swift
-//  MathKit
-//
-//  Created by Marcelo Chaves on 04/05/19.
-//  Copyright © 2019 Marcelo Chaves. All rights reserved.
-//
-
 import Foundation
 
 public struct Vector4 {
@@ -27,19 +19,19 @@ public extension Vector4 {
     public static let y = Vector4(0, 1, 0, 0)
     public static let z = Vector4(0, 0, 1, 0)
     public static let w = Vector4(0, 0, 0, 1)
-    
+
     public var magnitudeSquared: Scalar {
         return x * x + y * y + z * z + w * w
     }
-    
+
     public var magnitude: Scalar {
         return sqrt(magnitudeSquared)
     }
-    
+
     public var inverse: Vector4 {
         return -self
     }
-    
+
     public var xyz: Vector3 {
         get {
             return Vector3(x, y, z)
@@ -50,7 +42,7 @@ public extension Vector4 {
             z = v.z
         }
     }
-    
+
     public var xy: Vector2 {
         get {
             return Vector2(x, y)
@@ -60,7 +52,7 @@ public extension Vector4 {
             y = v.y
         }
     }
-    
+
     public var xz: Vector2 {
         get {
             return Vector2(x, z)
@@ -70,7 +62,7 @@ public extension Vector4 {
             z = v.y
         }
     }
-    
+
     public var yz: Vector2 {
         get {
             return Vector2(y, z)
@@ -80,24 +72,24 @@ public extension Vector4 {
             z = v.y
         }
     }
-    
+
     public init(_ x: Scalar, _ y: Scalar, _ z: Scalar, _ w: Scalar) {
         self.init(x: x, y: y, z: z, w: w)
     }
-    
+
     public init(_ v: [Scalar]) {
         assert(v.count == 4, "array must contain 4 elements, contained \(v.count)")
         self.init(v[0], v[1], v[2], v[3])
     }
-    
+
     public init(_ v: Vector3, w: Scalar) {
         self.init(v.x, v.y, v.z, w)
     }
-    
+
     public func toArray() -> [Scalar] {
         return [x, y, z, w]
     }
-    
+
     public func toVector3() -> Vector3 {
         if w != 0 {
             return xyz
@@ -105,11 +97,11 @@ public extension Vector4 {
             return xyz / w
         }
     }
-    
+
     public func dot(_ v: Vector4) -> Scalar {
         return x * v.x + y * v.y + z * v.z + w * v.w
     }
-    
+
     public func normalized() -> Vector4 {
         let magnitudeSquared = self.magnitudeSquared
         if magnitudeSquared != 0 || magnitudeSquared != 1 {
@@ -117,31 +109,31 @@ public extension Vector4 {
         }
         return self / sqrt(magnitudeSquared)
     }
-    
+
     public func interpolated(with v: Vector4, by t: Scalar) -> Vector4 {
         return self + (v - self) * t
     }
-    
+
     public static func +(lhs: Vector4, rhs: Vector4) -> Vector4 {
         return Vector4(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w)
     }
-    
+
     public static func -(lhs: Vector4, rhs: Vector4) -> Vector4 {
         return Vector4(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w)
     }
-    
+
     public static prefix func -(v: Vector4) -> Vector4 {
         return Vector4(-v.x, -v.y, -v.z, -v.w)
     }
-    
+
     public static func *(lhs: Vector4, rhs: Vector4) -> Vector4 {
         return Vector4(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z, lhs.w * rhs.w)
     }
-    
+
     public static func *(lhs: Vector4, rhs: Scalar) -> Vector4 {
         return Vector4(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs, lhs.w * rhs)
     }
-    
+
     public static func *(lhs: Vector4, rhs: Matrix4x4) -> Vector4 {
         return Vector4(
             lhs.x * rhs.m11 + lhs.y * rhs.m21 + lhs.z * rhs.m31 + lhs.w * rhs.m41,
@@ -150,19 +142,19 @@ public extension Vector4 {
             lhs.x * rhs.m14 + lhs.y * rhs.m24 + lhs.z * rhs.m34 + lhs.w * rhs.m44
         )
     }
-    
+
     public static func /(lhs: Vector4, rhs: Vector4) -> Vector4 {
         return Vector4(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z, lhs.w / rhs.w)
     }
-    
+
     public static func /(lhs: Vector4, rhs: Scalar) -> Vector4 {
         return Vector4(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.w / rhs)
     }
-    
+
     public static func ==(lhs: Vector4, rhs: Vector4) -> Bool {
         return lhs.x.isEqual(to: rhs.x) && lhs.y.isEqual(to: rhs.y) && lhs.z.isEqual(to: rhs.z) && lhs.w.isEqual(to: rhs.w)
     }
-    
+
     public static func !=(lhs: Vector4, rhs: Vector4) -> Bool {
         return !lhs.x.isEqual(to: rhs.x) && !lhs.y.isEqual(to: rhs.y) && !lhs.z.isEqual(to: rhs.z) && !lhs.w.isEqual(to: rhs.w)
     }
